dfe619b0d39c13b5307725ff058f2a662534fb72
Sultan <59149468+sultansozoev@users.noreply.github.com>
2024-03-11 13:11:56 +0100

    Update plyr.js

--- js/plyr.js
+++ js/plyr.js
@@ -13,8 +13,29 @@ const cardImage = document.getElementById('card-image');
 const sourceElement = videoPlayer.querySelectorAll('source')[0];
 const subtitle = document.getElementById('subtitle');
 const subtitleUrl = `${apiUrl}/subtitle?film=${encodeURI(film)}`;
-const player = new Plyr('video', { captions: { active: true } });
+const controls =
+  [
+    'play-large', // The large play button in the center
+    'restart', // Restart playback
+    'rewind', // Rewind by the seek time (default 10 seconds)
+    'play', // Play/pause playback
+    'fast-forward', // Fast forward by the seek time (default 10 seconds)
+    'progress', // The progress bar and scrubber for playback and buffering
+    'current-time', // The current time of playback
+    'duration', // The full duration of the media
+    'mute', // Toggle mute
+    'volume', // Volume control
+    'captions', // Toggle captions
+    'settings', // Settings menu
+    'pip', // Picture-in-picture (currently Safari only)
+    'airplay', // Airplay (currently Safari only)
+    'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+    'fullscreen' // Toggle fullscreen
+  ];
+
+const player = new Plyr('video', { captions: { active: true }, controls });
 player.elements.container.tabIndex = 0;
+player.config.urls.download = url;
 window.player = player;
 
 isPresent.addEventListener('change', function () {
