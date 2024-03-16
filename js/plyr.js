30980384d2f9fc360c19725c0669bd4a9f46546e
SULTAN SOZOEV <sozoev.sultan@gmail.com>
2024-03-16 14:10:16 +0100

    no is present

--- js/plyr.js
+++ js/plyr.js
@@ -2,13 +2,11 @@ import { apiUrl } from '/api/config.js';
 const queryParams = new URLSearchParams(window.location.search);
 const film = queryParams.get('film');
 let url = `${apiUrl}/video?film=${film}`;
-const id = queryParams.get('filmId');
 const videoPlayer = document.getElementById('player');
 const body = document.getElementById('body');
 const title = document.getElementById('title');
 const year = document.getElementById('year');
 const budget = document.getElementById('budget');
-const isPresent = document.getElementById('cb2');
 const cardImage = document.getElementById('card-image');
 const sourceElement = videoPlayer.querySelectorAll('source')[0];
 const subtitle = document.getElementById('subtitle');
@@ -38,31 +36,6 @@ player.elements.container.tabIndex = 0;
 player.config.urls.download = url;
 window.player = player;
 
-isPresent.addEventListener('change', function () {
-  const isPresentUrl = `${apiUrl}/isPresent`;
-  if (this.checked) {
-    fetch(isPresentUrl, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify({id_film: id, isPresent: 1})
-    }).catch(error => {
-      console.error('Error fetching films:', error);
-    });
-  } else {
-    fetch(isPresentUrl, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify({id_film: id, isPresent: 0})
-    }).catch(error => {
-      console.error('Error fetching films:', error);
-    });
-  }
-});
-
 fetch(url)
   .then(response => response.url)
   .then(videoUrl => {
@@ -90,12 +63,10 @@ fetch(url)
       budget.appendChild(document.createTextNode("non è ancora inserito questo dato nel database"));
     }
     title.appendChild(document.createTextNode(film.title));
-    if (film.isPresent === 1)
-      isPresent.setAttribute('checked', 'checked');
-    cardImage.setAttribute("style", `background-image: url("${film.poster_url}");`);
-    videoPlayer.setAttribute('data-poster', film.background);
+    cardImage.setAttribute("style", `background-image: url("${film.poster}");`);
+    videoPlayer.setAttribute('data-poster', film.background_image);
     setTimeout(() => {
-      player.poster = film.background;
+      player.poster = film.background_image;
     }, 500)
-    body.setAttribute("style", `background-image: url("${film.background}"); backdrop-filter: blur(5px);`);
+    body.setAttribute("style", `background-image: url("${film.background_image}"); backdrop-filter: blur(5px);`);
   });
