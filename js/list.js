2a7ebf0733b2010babc3400c80f4434e4c72e72e
Sultan <sozoev.sultan@gmail.com>
2024-04-02 02:58:06 +0200

    list

---
+++ js/list.js
@@ -0,0 +1,44 @@
+import { apiUrl } from '/api/config.js';
+
+function fetchList(url, container, user_id) {
+  fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({user_id})
+  })
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(request => {
+        const tr = document.createElement('tr');
+        const title = document.createElement('td');
+        if (request.title === 'undefined')
+          title.innerText = request.name;
+        else title.innerText = request.title;
+        const tipo = document.createElement('td');
+        tipo.innerText = request.type;
+        const tdb = document.createElement('td');
+        const button = document.createElement('button');
+        button.classList.add('btn');
+        button.classList.add('btn-danger');
+        button.classList.add('btn-sm');
+        button.innerHTML = `<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-archive\" viewBox=\"0 0 16 16\">
+                            <path d=\"M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5\"/>`;
+        button.setAttribute('onclick', `elimina('${request.list_id}', '${user_id}', '${request.request_id}')`)
+        tdb.appendChild(button);
+        tr.appendChild(title);
+        tr.appendChild(tipo);
+        tr.appendChild(tdb);
+        container.appendChild(tr);
+
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching films:', error);
+    });
+}
+
+const url = `${apiUrl}/getList`;
+const container = document.getElementById('container');
+fetchList(url, container, getCookie("user"));
--- js/request.js
+++ js/request.js
@@ -17,3 +17,25 @@ function addRequest(name, title, request_idm, type, user_id) {
       console.error('Error fetching films:', error);
     });
 }
+
+function elimina(list_id, user_id, request_id) {
+  const url = `https://surio.ddns.net/elimina`;
+  fetch(url, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body:  JSON.stringify({
+      list_id,
+      user_id,
+      request_id
+    })
+  })
+    .then(response => response.json())
+    .then(res => {
+      window.location.reload();
+    })
+    .catch(error => {
+      console.error('Error fetching films:', error);
+    });
+}
