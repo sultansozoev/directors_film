0496962758f63a5d20c6873ca587d04c05aca5b6
SULTAN SOZOEV <sozoev.sultan@gmail.com>
2024-04-06 21:35:15 +0200

    pagination

--- js/slider.js
+++ js/slider.js
@@ -1,34 +1,37 @@
-const swiper = new Swiper(".slide-container", {
-  slidesPerView: 1,
-  spaceBetween: 5,
-  sliderPerGroup: 6,
-  loop: true,
-  centerSlide: "true",
-  touchEventsTarget: 'container',
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+for (let i = 1; i < 14; i++) {
+  new Swiper(".slide-container"+i, {
+    slidesPerView: 4,
+    spaceBetween: 5,
+    sliderPerGroup: 6,
     dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    loop: true,
+    centerSlide: "true",
+    touchEventsTarget: 'container',
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination"+i,
+      dynamicBullets: true,
+      loop: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next'+i,
+      prevEl: '.swiper-button-prev'+i,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
 
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  },
-});
+}
