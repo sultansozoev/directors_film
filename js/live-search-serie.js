99c769b98a13f8e163a33ed0da14bb4202674677
SULTAN SOZOEV <sozoev.sultan@gmail.com>
2024-05-01 00:00:43 +0200

    scrollIntoView

--- js/live-search-serie.js
+++ js/live-search-serie.js
@@ -1,7 +1,6 @@
 import { apiUrl } from '/api/config.js';
 import { searchTitle } from './mainFunctions.js';
 
-const banner = document.getElementById('banner');
 const searchBar = document.querySelector("[data-search]");
 const results_container = document.getElementById("results-container");
 results_container.innerText = "";
@@ -9,12 +8,9 @@ results_container.innerText = "";
 searchBar.addEventListener("input", (event) => {
   let searchValue = event.target.value.trim().toLowerCase();
   results_container.innerText = "";
-  if (searchValue.length === 0 || searchValue === '') {
-    banner.style.display = 'block';
+  if (searchValue.length === 0 || searchValue === '')
     return;
-  } else {
-    banner.style.display = 'none';
-  }
+  results_container.scrollIntoView({ behavior: "smooth" });
   const url = `${apiUrl}/searchSerie?title=` + searchValue;
   const urlPlayer = `player_serie.html?serie=`;
   searchTitle(url, results_container, urlPlayer, 'serie_tv_id');
--- js/live-search.js
+++ js/live-search.js
@@ -1,7 +1,6 @@
 import { apiUrl } from '/api/config.js';
 import { searchTitle } from './mainFunctions.js';
 
-const banner = document.getElementById('banner');
 const searchBar = document.querySelector("[data-search]");
 const results_container = document.getElementById("results-container");
 results_container.innerText = "";
@@ -9,12 +8,9 @@ results_container.innerText = "";
 searchBar.addEventListener("input", (event) => {
   let searchValue = event.target.value.trim().toLowerCase();
   results_container.innerText = "";
-  if (searchValue.length === 0 || searchValue === '') {
-    banner.style.display = 'block';
+  if (searchValue.length === 0 || searchValue === '')
     return;
-  } else {
-    banner.style.display = 'none';
-  }
+  results_container.scrollIntoView({ behavior: "smooth" });
   const url = `${apiUrl}/search?title=` + searchValue;
   const urlPlayer = `new-player.html?film=`;
   searchTitle(url, results_container, urlPlayer,'movie_id');
