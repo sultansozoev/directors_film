618fb6991ba2ecd0de523cee92c8a9d4c315b833
Sultan <sozoev.sultan@gmail.com>
2024-11-04 15:32:42 +0100

    fix bug search

--- js/live-search-serie.js
+++ js/live-search-serie.js
@@ -3,6 +3,7 @@ import { searchTitle } from './mainFunctions.js';
 
 const searchBar = document.querySelector("[data-search]");
 const results_container = document.getElementById("results-container");
+const container1 = document.getElementById("container1");
 results_container.innerText = "";
 
 searchBar.addEventListener("input", (event) => {
@@ -10,7 +11,7 @@ searchBar.addEventListener("input", (event) => {
   results_container.innerText = "";
   if (searchValue.length === 0 || searchValue === '')
     return;
-  results_container.scrollIntoView({ behavior: "smooth" });
+  container1.scrollIntoView({ behavior: "smooth" });
   const url = `${apiUrl}/searchSerie?title=` + searchValue;
   const urlPlayer = `player_serie.html?serie=`;
   searchTitle(url, results_container, urlPlayer, 'serie_tv_id');
