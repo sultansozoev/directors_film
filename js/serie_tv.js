634597004bb05a7da2454d350981ce6b89358a4d
Sultan <sozoev.sultan@gmail.com>
2025-02-10 01:18:32 +0100

    fixing

--- js/serie_tv.js
+++ js/serie_tv.js
@@ -120,7 +120,7 @@ async function removeFavorite(serieTV, icon) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        serie_tv_id: serieTV.serie_tv_id,
+        movie_id: serieTV.serie_tv_id,
         user_id: user_id
       })
     });
